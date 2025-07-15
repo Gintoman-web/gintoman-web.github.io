fb0b04364aecf17b97ee68f899fa63848d5bf888
Gintoman-web <gintokiletsplay@gmail.com>
2025-07-15 23:31:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const mainNav = document.querySelector('.main-nav')
+
+burgerMenu.addEventListener('click', () => {
+    mainNav.classList.toggle('active')
+});
